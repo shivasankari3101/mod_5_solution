8a438c5683b27d87bc708f9b25ac08121f58f6ac
SHIVA SANKARI C <54460360+shivasankari3101@users.noreply.github.com>
2020-07-16 22:38:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -62,15 +62,7 @@ var switchMenuToActive = function () {
 
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
-  showLoading("#main-content");
-$ajaxUtils.sendGetRequest(
-  homeHtmlUrl,
-  function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
-  },
-  false);
-});
+  
 
 // TODO: STEP 0: Look over the code from
 // *** start ***
@@ -110,7 +102,9 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName =function chooseRandomCategory (categories);
+      var chosenCategoryShortName =chooseRandomCategory (categories);
+      chosenCategoryShortName=chosenCategoryShortName.short_name; 
+      
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
